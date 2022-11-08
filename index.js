7bf5a73aaa3f5ee91614f79d49df9827710478c0
tahdi-islam <tshdi60@gmail.com>
2022-11-08 14:54:05 +0600

    create get service method

--- index.js
+++ index.js
@@ -1,40 +1,55 @@
-const express = require('express');
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-require("dotenv").config()
-const app = express()
+require("dotenv").config();
+const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // connect db
 
-
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.rgyxe1r.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-    try{
-        const Services = client.db("Snackdeckdb").collection("services");
-        app.post("/services", async(req, res) => {
-            const service = req.body;
-            const result = await Services.insertOne(service)
-            res.status(200).send({result})
-        })
-    }
-    finally{
-        
-    }
+async function run() {
+  try {
+    const Services = client.db("Snackdeckdb").collection("services");
+    app.get("/services", async (req, res) => {
+      const dataLimit = parseInt(req.query.limit);
+
+      const cursor = Services.find({});
+      // const result = await cursor.limit(dataLimit).toArray()
+      // res.send({result})
+      // let
+      if (dataLimit) {
+        const storedServices = await cursor.limit(dataLimit).toArray();
+        res.send({ storedServices });
+      } else {
+        const storedServices = await cursor.toArray();
+        res.send({ storedServices });
+      }
+    });
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+      const result = await Services.insertOne(service);
+      res.status(200).send({ result });
+    });
+  } finally {
+  }
 }
-run().catch(err => console.log(err))
-
+run().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-    res.send("SnackDeck server is running.")
-})
+  res.send("SnackDeck server is running.");
+});
 
 app.listen(port, () => {
-    console.log(`SnackDeck server is running on port ${port}`);
-})
+  console.log(`SnackDeck server is running on port ${port}`);
+});
